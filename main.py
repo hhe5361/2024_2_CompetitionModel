1a71cb8809b5ad29b1492381c5663148eb3fc6a0
HwangHyoeun <hhe5361@naver.com>
2024-11-28 23:42:19 +0900

    EfficientNet-B0.

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import random
 import torch
 from torch import nn, optim
 from torch.optim import lr_scheduler
-from model import EfficientNet
 from utils import checkParams, train_model, evaluate
 from optimTarget import learning_rate, training_epochs, schedule_steps
+from model.model import EfficientNet
+from model.utils import get_model_params
 
 seed = 3334
 torch.manual_seed(seed)
@@ -19,8 +20,15 @@ np.random.seed(seed)
 random.seed(seed)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model_ft = EfficientNet(num_classes=10).cuda()
-test_model = EfficientNet(num_classes=10).cuda()
+
+# 모델 매개변수 가져오기
+blocks_args, global_params = get_model_params('efficientnet-b0', override_params={'include_top': False})
+
+model_ft = EfficientNet(blocks_args, global_params).cuda()
+model_ft._fc = nn.Linear(global_params['width_coefficient'] * 1280, 10)  
+
+test_model = EfficientNet(blocks_args, global_params).cuda()
+test_model._fc = nn.Linear(global_params['width_coefficient'] * 1280, 10) 
 
 checkParams(model_ft)
 print(device)
