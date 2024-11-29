7fdd828c761217f21268a083dcb4b28de8daa98b
HwangHyoeun <hhe5361@naver.com>
2024-11-30 03:41:36 +0900

    denseNet with pytorch code upload

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 # importing all the libraries we need
 import torch
+from torchvision.models import densenet121
 from torch import nn, optim
 from torch.optim import lr_scheduler
 from optimTarget import learning_rate, training_epochs, schedule_steps
-from model.model import EfficientNet
-from model.utils import get_model_params
 from utils import checkParams, train_model, evaluate
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model_ft = EfficientNet.from_name("efficientnet-b0")
-test_model = EfficientNet.from_name("efficientnet-b0")
+model_ft = densenet121(pretrained=False).cuda()
+test_model = densenet121(pretrained=False).cuda()
 
-num_ftrs = model_ft._fc.in_features
-model_ft._fc = nn.Linear(num_ftrs, 10)
-test_model._fc = nn.Linear(num_ftrs, 10)
-
-model_ft= model_ft.cuda()
-test_model = test_model.cuda()
+model_ft.classifier = nn.Linear(model_ft.classifier.in_features, 10)
+test_model.classifier = nn.Linear(model_ft.classifier.in_features, 10)
 
 checkParams(model_ft)
 print(device)
@@ -25,9 +20,7 @@ model_ft = model_ft.to(device)
 
 # Optimization Loss function
 criterion = nn.CrossEntropyLoss()
-
 optimizer_ft = optim.SGD(model_ft.parameters(), lr=learning_rate, momentum=0.9)
-
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=schedule_steps, gamma=0.1)
 
 
